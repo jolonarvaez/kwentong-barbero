c55e86d3cb158cc315d4dfab758863b702a19531
migsmo <97015261+migsmo@users.noreply.github.com>
2024-07-12 01:17:44 +0800

    Create product-card.tsx

---
+++ components/reusables/product-card.tsx
@@ -0,0 +1,20 @@
+interface Props {
+  imageSrc: string;
+  alt?: string;
+  productName: string;
+  price: string;
+}
+export default function ProductCard({
+  imageSrc,
+  alt,
+  productName,
+  price,
+}: Props) {
+  return (
+    <div>
+      <img src={imageSrc} alt={alt || ''} />
+      <h3 className='font-bold'>{productName}</h3>
+      <p>P{price}</p>
+    </div>
+  );
+}
