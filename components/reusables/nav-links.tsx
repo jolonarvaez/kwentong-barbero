1afd2337be69e4a01213710ec6f123fd2fa2420c
migsmo <97015261+migsmo@users.noreply.github.com>
2024-07-12 00:54:05 +0800

    Added leftSideIcon to nav-links.tsx

--- components/reusables/nav-links.tsx
+++ components/reusables/nav-links.tsx
@@ -1,20 +1,39 @@
-import Link from "next/link";
-import { PropsWithChildren } from "react";
+import Link from 'next/link';
+import { CSSProperties, PropsWithChildren } from 'react';
 
 interface Props extends PropsWithChildren {
   href: string;
   className?: string;
+  style?: CSSProperties | undefined;
+  leftSideIcon?: string;
 }
 
-export default function NavLink({ href, className, children }: Props) {
+export default function NavLink({
+  href,
+  className,
+  style,
+  leftSideIcon,
+  children,
+}: Props) {
   return (
     <Link
       href={href}
-      className={`hover:opacity-70 transition-opacity ${
-        className ? className : ""
-      }`}
+      className={`hover:opacity-70 transition-opacity ${className || ''}`}
+      style={style}
     >
-      {children}
+      <div className='flex flex-row content-center'>
+        {leftSideIcon && (
+          <img
+            src={leftSideIcon}
+            alt='facebook-logo'
+            className='mr-2'
+            style={{
+              objectFit: 'contain',
+            }}
+          />
+        )}
+        {children}
+      </div>
     </Link>
   );
 }
