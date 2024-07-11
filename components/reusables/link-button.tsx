b6ec28c111c846f798ad81855ce377f3f3beeef8
migsmo <97015261+migsmo@users.noreply.github.com>
2024-07-12 00:53:56 +0800

    Added leftSideIcon to link-button.tsx

--- components/reusables/link-button.tsx
+++ components/reusables/link-button.tsx
@@ -1,13 +1,27 @@
-import { PropsWithChildren } from "react";
-import NavLink from "./nav-links";
+import { CSSProperties, PropsWithChildren } from 'react';
+import NavLink from './nav-links';
 
 interface Props extends PropsWithChildren {
   href: string;
+  style?: CSSProperties | undefined;
+  className?: string | undefined;
+  leftSideIcon?: string;
 }
 
-export default function LinkButton({ href, children }: Props) {
+export default function LinkButton({
+  href,
+  style,
+  className,
+  children,
+  leftSideIcon,
+}: Props) {
   return (
-    <NavLink href={href} className="bg-[#A38748] text-white px-6 py-2 rounded">
+    <NavLink
+      href={href}
+      style={style}
+      className={`bg-[#A38748] text-white px-6 py-2 rounded ${className || ''}`}
+      leftSideIcon={leftSideIcon}
+    >
       {children}
     </NavLink>
   );
