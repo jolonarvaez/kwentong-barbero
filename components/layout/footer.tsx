f613209e24a8fe3dca37e28728c4bec858533882
jolonarvaez <jose_wilfredo_narvaez@dlsu.edu.ph>
2024-07-21 17:18:28 +0800

    added partial footer

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,3 +1,25 @@
+import Image from "next/image";
+
 export default function Footer() {
-  return <footer className="h-[256px] bg-dark-blue tex-twhite">Hello world</footer>;
+  return (
+    <footer className="flex flex-col justify-center h-[256px] bg-dark-blue text-white px-[104px] space-y-[32px]">
+      <Image
+        src="/kb-logo.png"
+        alt="Kwentong Barbero"
+        width={147.25}
+        height={75}
+        style={{
+          height: "auto",
+        }}
+      />
+      <div
+        className="text-white"
+        style={{
+          fontSize: "0.875rem",
+        }}
+      >
+        Copyright 2024 Kwentong Barbero. Created by Bord Studios
+      </div>
+    </footer>
+  );
 }
